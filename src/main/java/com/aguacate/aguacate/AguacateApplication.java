472f82cc41945a71ab2dd05330af74a73e069def
Fer_Carraro <carraro.fernando@gmail.com>
2020-08-08 21:25:09 -0500

    Proyecto Spring

---
+++ src/main/java/com/aguacate/aguacate/AguacateApplication.java
@@ -0,0 +1,13 @@
+package com.aguacate.aguacate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguacateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguacateApplication.class, args);
+	}
+
+}
