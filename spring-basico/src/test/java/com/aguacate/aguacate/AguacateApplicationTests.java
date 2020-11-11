c5cf69380807af988c686f6cc1e1f59f85278292
Fer_Carraro <carraro.fernando@gmail.com>
2020-11-11 13:37:16 -0600

    Se han hecho cambios importantes

---
+++ spring-basico/src/test/java/com/aguacate/aguacate/AguacateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguacate.aguacate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguacateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
