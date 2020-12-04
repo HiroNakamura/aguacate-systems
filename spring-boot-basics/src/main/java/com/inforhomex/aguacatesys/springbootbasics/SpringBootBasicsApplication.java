e2c55c4c783d9db3f9a8baae6a3a0fa6864c1861
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-04 14:42:55 -0600

    Se creo proyecto spring-boot-basics

---
+++ spring-boot-basics/src/main/java/com/inforhomex/aguacatesys/springbootbasics/SpringBootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.inforhomex.aguacatesys.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicsApplication.class, args);
+	}
+
+}
