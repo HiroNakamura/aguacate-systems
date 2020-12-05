b4ac8ef6d21a2f5323bfcfaff231351889cd7ac9
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-05 10:48:30 -0600

    Se han hecho cambios en my-beans.xml, application.properties, y se agregaron nuevas clases: Empresa y MySite

---
+++ spring-basico/src/main/java/com/aguacate/aguacate/model/Empresa.java
@@ -0,0 +1,44 @@
+package com.aguacate.aguacate.model;
+
+public class Empresa{
+    private String nombre;
+    private MySite mySite;
+
+    public Empresa(){}
+
+    public Empresa(String nombre, String mySite){
+        super();
+        this.mySite = mySite;
+        this.nombre = nombre;
+    }
+
+    public void init(){
+        System.out.println("Se inicia bean Empresa");
+    }
+
+    public void destroy(){
+        System.out.println("Se destruye bean Empresa");
+    }
+
+    public MySite getMySite(){
+        return mySite;
+    }
+
+    public void setMySite(MySite mySite){
+        this.mySite = mySite;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString(){
+        return this.nombre +", URL: "+this.mySite;
+    }
+
+}
---
+++ spring-basico/src/main/java/com/aguacate/aguacate/model/MySite.java
@@ -0,0 +1,44 @@
+package com.aguacate.aguacate.model;
+
+public class MySite{
+    private String url;
+    private String nombre;
+
+    public MySite(){}
+
+    public MySite(String url, String nombre){
+        super();
+        this.url = url;
+        this.nombre = nombre;
+    }
+
+    public void init(){
+        System.out.println("Se inicia bean MySite");
+    }
+
+    public void destroy(){
+        System.out.println("Se destruye bean MySite");
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString(){
+        return this.url +" : "+this.nombre;
+    }
+
+}
