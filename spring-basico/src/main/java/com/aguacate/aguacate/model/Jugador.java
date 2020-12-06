1cbc577931126d1a4af7d4abc56c750a144593ae
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-06 12:16:40 -0600

    Se ha editado spring-beans.xml, Jugador.java, Napoles.java, se agregaron anotaciones @Value, @Component

--- spring-basico/src/main/java/com/aguacate/aguacate/model/Jugador.java
+++ spring-basico/src/main/java/com/aguacate/aguacate/model/Jugador.java
@@ -1,14 +1,20 @@
 package com.aguacate.aguacate.model;
 
+import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
 import com.aguacate.aguacate.interfaces.IEquipo;
 
 
+@Component
 public class Jugador{
 	private int numero;
 	private String nombre;
+
+	@Value("450")
+	private int goles;
 	
 	//Con la anotacion @Autowired estamos inyectando la dependencia
 	//Con la anotacion @Qualifier estamos especificando el bean a usar
@@ -26,6 +32,14 @@ public class Jugador{
 		this.equipo = equipo;
 	}
 
+	public Jugador(int numero, String nombre,int goles, IEquipo equipo){
+		super();
+		this.numero = numero;
+		this.nombre = nombre;
+		this.equipo = equipo;
+		this.goles = goles;
+	}
+
 	public Jugador(int numero, String nombre){
 		super();
 		this.numero = numero;
@@ -38,6 +52,14 @@ public class Jugador{
 
 	public void setNumero(int numero){
 		this.numero = numero;
+	}
+
+	public int getGoles(){
+		return goles;
+	}
+
+	public void setGoles(int goles){
+		this.goles = goles;
 	}
 
 	public String getNombre(){
@@ -65,7 +87,7 @@ public class Jugador{
 
 	@Override
 	public String toString(){
-		return "Jugador{numero: "+this.numero+", nombre:"+this.nombre+"}";
+		return "Jugador{numero: "+this.numero+", nombre:"+this.nombre+", no. goles: "+this.goles+"}";
 	}
 
 }
--- spring-basico/src/main/java/com/aguacate/aguacate/model/Napoles.java
+++ spring-basico/src/main/java/com/aguacate/aguacate/model/Napoles.java
@@ -1,8 +1,10 @@
 package com.aguacate.aguacate.model;
 
-
+import org.springframework.stereotype.Component;
 import com.aguacate.aguacate.interfaces.IEquipo;
 
+
+@Component
 public class Napoles implements IEquipo{
 	
 	@Override
