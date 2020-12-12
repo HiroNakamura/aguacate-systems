16f04ac293c4278f0e75efe5ab9b56e9728e559d
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-12 16:05:00 -0600

    Se agrego carpeta tutoriales y un proyecto de spring jms

---
+++ tutoriales/gs-messaging-jms/src/main/java/hello/Application.java
@@ -0,0 +1,47 @@
+package hello;
+
+import javax.jms.ConnectionFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+	private static final String MENSAJE = "\tFelicitaciones, Fernando!!!\n Has obtenido el premio Pulitzer!!!";
+	private static final String EMAIL = "educar.inforhomex@inforhomex.org";
+
+	@Bean
+	public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory, DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(factory, connectionFactory);
+		return factory;
+	}
+
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+		System.out.println("Enviando email...");
+		jmsTemplate.convertAndSend("mailbox", new Email(EMAIL, MENSAJE));
+	}
+
+}
