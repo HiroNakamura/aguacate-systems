a60815039d9a788c1506e228f2b79209d9d78880
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-12 22:07:12 -0600

    pom.xml editado; SE edito clase Customer.java y se agrefo appliaction.yml

--- tutoriales/gs-accessing-data-mongodb/src/main/java/com/example/accessingdatamongodb/Customer.java
+++ tutoriales/gs-accessing-data-mongodb/src/main/java/com/example/accessingdatamongodb/Customer.java
@@ -1,20 +1,52 @@
 package com.example.accessingdatamongodb;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import javax.validation.constraints.NotNull;
 
 
+@Document(collection = "customer")
+@JsonPropertyOrder({"_id", "firstname","lastname"})
 public class Customer {
 
 	@Id
 	public String id;
-
 	public String firstName;
 	public String lastName;
 
 	public Customer() {}
 
 	public Customer(String firstName, String lastName) {
+		super();
 		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public void setId(String id){
+		this.id=id;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public void setLastName(String lastName){
 		this.lastName = lastName;
 	}
 
