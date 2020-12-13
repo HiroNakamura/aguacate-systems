50b38c7b9448c8c52077352acd19a7b353425e34
Fer_Carraro <carraro.fernando@gmail.com>
2020-12-12 20:40:21 -0600

    Se han agregado nuevo proyecto y clases

---
+++ tutoriales/mqspring/src/main/java/org/inforhomex/mqspring/MqspringApplication.java
@@ -0,0 +1,13 @@
+package org.inforhomex.mqspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqspringApplication.class, args);
+	}
+
+}
